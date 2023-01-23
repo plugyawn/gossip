bbedc1f9e19e368ec86a96051c4b13748aedd771
Progyan <progyan.das@iitgn.ac.in>
2023-01-23 17:24:08 +0530

    core.py, test.py, utils/datatypes.py: Modularized the code into different files, added tests, fixed inheritence issues.

---
+++ utils/datatypes.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from fractions import Fraction
+from typing import Union, Mapping
+
+@dataclass
+class NumLiteral:
+    value: Fraction
+    def __init__(self, *args):
+        self.value = Fraction(*args)
+
+@dataclass
+class BinOp:
+    operator: str
+    left: 'AST'
+    right: 'AST'
+
+@dataclass
+class UnOp:
+    operator: str
+    right: 'AST'
+
+@dataclass
+class Variable:
+    name: str
+
+@dataclass
+class Let:
+    var: 'AST'
+    e1: 'AST'
+    e2: 'AST'
+
+@dataclass
+class If:
+    cond: 'AST'
+    e1: 'AST'
+    e2: 'AST'
+
+@dataclass
+class BoolLiteral:
+    value: bool
+
+AST = NumLiteral | BinOp | UnOp | Variable | Let | BoolLiteral | If
+
+Value = Fraction
+
+class InvalidProgram(Exception):
+    pass
