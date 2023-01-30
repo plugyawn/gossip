ab8eeef9feaba788a396e6a7c834337f034d4966
Dhyey Thummar <dhyeykumar.thummar@iitgn.ac.in>
2023-01-30 14:54:22 +0530

    test.py: Imported necessary datatypes.

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from fractions import Fraction
 from typing import Union, Mapping
 from core import RuntimeEnvironment
-from utils.datatypes import AST, NumLiteral, BinOp, Variable, Let, Value, InvalidProgram, If, BoolLiteral, UnOp, ASTSequence
+from utils.datatypes import AST, NumLiteral, BinOp, Variable, Let, Value, InvalidProgram, If, BoolLiteral, UnOp, ASTSequence, NumType, BoolType
 from utils.typechecker import StaticTypeChecker
 
 
