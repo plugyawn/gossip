01d856c57c319cc6ed944f27691bb235eea5b505
Progyan <progyan.das@iitgn.ac.in>
2023-01-30 19:20:13 +0530

    errors.py: Added necessary Exception classes.

--- utils/errors.py
+++ utils/errors.py
@@ -10,4 +10,31 @@ class DefinitionError(Exception):
     def __init__(self, name):
         self.name = name
         print(f"DefinitionError: {name} is not defined.")
-        
+
+class InvalidProgramError(Exception):
+    """
+    Raised when a program is invalid.
+    """
+
+    def __init__(self, message, verbose = True):
+        self.message = message
+        print(f"InvalidProgramError: {message}")
+class EndOfStream(Exception):
+    """
+    Raised when the end of a stream is reached.
+    """
+    pass
+
+class EndOfTokens(Exception):
+    """
+    Raised when the end of a stream of tokens is reached.
+    """
+    pass
+
+class TokenError(Exception):
+    """
+    Raised when the end of a stream of tokens is reached.
+    """
+    pass
+
+
