180f9ec7f20df61090b5fd752fb69aef5e617d58
Dhyey Thummar <dhyeykumar.thummar@iitgn.ac.in>
2023-01-30 22:17:18 +0530

    typechecking: fixed everything.

--- test.py
+++ test.py
@@ -2,30 +2,40 @@ from dataclasses import dataclass
 from fractions import Fraction
 from typing import Union, Mapping
 from core import RuntimeEnvironment
-from utils.datatypes import *
+from utils.datatypes import AST, NumLiteral, BinOp, Variable, Let, Value, If, BoolLiteral, UnOp, ASTSequence, NumType, BoolType
+from utils.typechecker import StaticTypeChecker
 
 
 def test_eval():
     """
     Tests the eval method of the runtime environment.
     """
+    checker = StaticTypeChecker()
     runtime = RuntimeEnvironment()
     e1 = NumLiteral(2)
     e2 = NumLiteral(7)
     e3 = NumLiteral(9)
     e4 = NumLiteral(5)
-    e5 = BinOp("+", e2, e3)
-    e6 = BinOp("/", e5, e4)
-    e7 = BinOp("*", e1, e6)
+    e5 = BinOp("+", e2, e3) # 16
+    e6 = BinOp("/", e5, e4) # 3.2
+    e7 = BinOp("*", e1, e6) # 6.4
+    assert checker.check(e7).type == NumType()
+    e5 = BinOp("+", e2, e3) # 16
+    e6 = BinOp("/", e5, e4) # 3.2
+    e7 = BinOp("*", e1, e6) # 6.4
+    assert checker.check(e7).type == NumType()
     assert runtime.eval(e7) == Fraction(32, 5)
 
     e8 = UnOp("-", e7)
+    assert checker.check(e8).type == NumType()
+    assert checker.check(e8).type == NumType()
     assert runtime.eval(e8) == Fraction(-32, 5)
 
 def test_let_eval():
     """
     Tests the eval method of the runtime environment with the Let paradigm.
     """
+    checker = StaticTypeChecker()
     runtime = RuntimeEnvironment()
     a  = Variable("a")
     e1 = NumLiteral(5)
@@ -42,15 +52,18 @@ def test_bool_eval():
     """
     Tests the eval method of the runtime environment with boolean expressions.
     """
+    checker = StaticTypeChecker()
     runtime = RuntimeEnvironment()
     a = NumLiteral(5)
     b = NumLiteral(6)
     c = NumLiteral(1)
 
     e = BinOp("==", a, BinOp("-", b, c))
+    assert checker.check(e).type == BoolType()
     assert runtime.eval(e) == True
 
     e = BinOp("!=", a, BinOp("-", b, c))
+    assert checker.check(e).type == BoolType()
     assert runtime.eval(e) == False
 
     e = If(BinOp("==", a, BinOp("-", b, c)), NumLiteral(1), NumLiteral(2))
--- utils/errors.py
+++ utils/errors.py
@@ -10,4 +10,24 @@ class DefinitionError(Exception):
     def __init__(self, name):
         self.name = name
         print(f"DefinitionError: {name} is not defined.")
-        
+
+class InvalidProgramError(Exception):
+    """
+    Raised when a program is invalid.
+    """
+
+    def __init__(self, program):
+        self.program = program
+        print(f"InvalidProgramError: Runtime environment does not support program: {program}.")
+
+class TypeCheckError(Exception):
+    """
+    Raised when the type of the operands in the operation are not of valid type
+    """
+
+    def __init__(self, oprtype = None, message:str = None):
+        self.oprtype = str(oprtype)
+        if not message:
+            print(f"TypeError: Operand(s) should have the type: {oprtype}.")
+        else:
+            print(f"TypeError: {message}")
