e9fc58067cab71bdd3d2e3992768c001aa6d1e53
Sukruta02 <76052389+SukrutaPrakashMidigeshi@users.noreply.github.com>
2023-01-31 21:56:36 +0530

    test for for loop, plus the scope list
    Signed-off-by: Sukruta02 <76052389+SukrutaPrakashMidigeshi@users.noreply.github.com>

--- test.py
+++ test.py
@@ -2,33 +2,42 @@ from dataclasses import dataclass
 from fractions import Fraction
 from typing import Union, Mapping
 from core import RuntimeEnvironment
-from utils.datatypes import *
+from utils.datatypes import AST, NumLiteral, BinOp, Variable, Let, Value, If, BoolLiteral, UnOp, ASTSequence, NumType, BoolType, ForLoop
+from utils.typechecker import StaticTypeChecker
 
 
 def test_eval():
     """
     Tests the eval method of the runtime environment.
     """
+    checker = StaticTypeChecker()
     runtime = RuntimeEnvironment()
     e1 = NumLiteral(2)
     e2 = NumLiteral(7)
     e3 = NumLiteral(9)
     e4 = NumLiteral(5)
-    e5 = BinOp("+", e2, e3)
-    e6 = BinOp("/", e5, e4)
-    e7 = BinOp("*", e1, e6)
+    e5 = BinOp("+", e2, e3) # 16
+    e6 = BinOp("/", e5, e4) # 3.2
+    e7 = BinOp("*", e1, e6) # 6.4
+    assert checker.check(e7).type == NumType()
+    e5 = BinOp("+", e2, e3) # 16
+    e6 = BinOp("/", e5, e4) # 3.2
+    e7 = BinOp("*", e1, e6) # 6.4
+    assert checker.check(e7).type == NumType()
     assert runtime.eval(e7) == Fraction(32, 5)
 
     e8 = UnOp("-", e7)
+    assert checker.check(e8).type == NumType()
+    assert checker.check(e8).type == NumType()
     assert runtime.eval(e8) == Fraction(-32, 5)
 
 def test_let_eval():
-    """
-    Tests the eval method of the runtime environment with the Let paradigm.
-    """
+    
     runtime = RuntimeEnvironment()
     a  = Variable("a")
     e1 = NumLiteral(5)
+    e4 = NumLiteral(10)
+    e7 = NumLiteral(4)
     e2 = BinOp("+", a, a)
     e  = Let(a, e1, BinOp("+", a, Let(a, e2, e2)))
     assert runtime.eval(e) == 25
@@ -37,20 +46,29 @@ def test_let_eval():
     e3 = NumLiteral(6)
     e  = BinOp("+", Let(a, e1, e2), Let(a, e3, e2))
     assert runtime.eval(e) == 22
+    e10 = Let(a,e4,e2)
+    e8 = BinOp("*",a,e7)
+    e9 = BinOp("==",e10,e8)
+    e = Let(a, e1, e9)
+    assert runtime.eval(e) == True
+    
 
 def test_bool_eval():
     """
     Tests the eval method of the runtime environment with boolean expressions.
     """
+    checker = StaticTypeChecker()
     runtime = RuntimeEnvironment()
     a = NumLiteral(5)
     b = NumLiteral(6)
     c = NumLiteral(1)
 
     e = BinOp("==", a, BinOp("-", b, c))
+    assert checker.check(e).type == BoolType()
     assert runtime.eval(e) == True
 
     e = BinOp("!=", a, BinOp("-", b, c))
+    assert checker.check(e).type == BoolType()
     assert runtime.eval(e) == False
 
     e = If(BinOp("==", a, BinOp("-", b, c)), NumLiteral(1), NumLiteral(2))
