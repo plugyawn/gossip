6e70c3f769077639a1e2299b258de4b0499e5495
Sukruta02 <76052389+SukrutaPrakashMidigeshi@users.noreply.github.com>
2023-01-31 21:57:25 +0530

    update for loop and correct error in main branch
    Signed-off-by: Sukruta02 <76052389+SukrutaPrakashMidigeshi@users.noreply.github.com>

--- core.py
+++ core.py
@@ -1,8 +1,8 @@
 from dataclasses import dataclass
 from fractions import Fraction
 from typing import Union, Mapping
-from utils.datatypes import AST, NumLiteral, BinOp, Variable, Let, Value, InvalidProgram, If, BoolLiteral, UnOp, ASTSequence, ForLoop
-from utils.errors import DefinitionError
+from utils.datatypes import AST, NumLiteral, BinOp, Variable, Let, Value, If, BoolLiteral, UnOp, ASTSequence, ForLoop
+from utils.errors import DefinitionError, InvalidProgramError
 
 class RuntimeEnvironment():
     """
@@ -11,27 +11,29 @@ class RuntimeEnvironment():
     recursively.
     """
     def __init__(self):
-        self.environment = {}
+        self.environment = []
+        self.environment.append({})
+        self.scope = 0 
 
-    def eval(self, program: AST or ASTSequence, environment = None) -> Value:
+    def eval(self, program: AST or ASTSequence) -> Value:
         """
         Recursively evaluates an AST or ASTSequence, returning a Value.
         By default, retains the environment from the runtime environment.
         However, you can pass in an environment to override this.
         """
-        if environment:
-            self.environment = environment
-        if not self.environment:
-            self.environment = {}
-        
         match program:
 
             case NumLiteral(value):
                 return value
 
             case Variable(name):
-                if name in self.environment:
-                    return self.environment[name]
+                scope_1 = self.scope
+                while ( len(self.environment) < (scope_1+1)):
+                    scope_1 = scope_1 - 1
+                while(scope_1>=0):
+                        if name in self.environment[scope_1]:
+                            return self.environment[scope_1][name]
+                        scope_1 = scope_1 - 1
 
                 raise DefinitionError(name)
 
@@ -51,34 +53,34 @@ class RuntimeEnvironment():
                 to the environment, then evaluates e2 with the new environment.
                 """
                 v1 = self.eval(e1)
-                return self.eval(e2, self.environment | { name: v1 })
+                self.scope = self.scope + 1
+                dict = {name : v1}
+                self.environment.append(dict)
+                val = self.eval(e2)
+                self.environment.pop()
+                self.scope = self.scope - 1
+                return(val)
 
             # Binary operations are all the same, except for the operator.
             case BinOp("+", left, right):
-                environment = self.environment
-                left = self.eval(left, environment)
-                right = self.eval(right, environment)
-                assert type(left) == type(right) == Fraction
+                left = self.eval(left)
+                right = self.eval(right)
                 return left + right
             case BinOp("-", left, right):
-                left = self.eval(left, environment)
-                right = self.eval(right, environment)
-                assert type(left) == type(right) == Fraction
+                left = self.eval(left)
+                right = self.eval(right)
                 return left - right
             case BinOp("*", left, right):
-                left = self.eval(left, environment)
-                right = self.eval(right, environment)
-                assert type(left) == type(right) == Fraction
+                left = self.eval(left)
+                right = self.eval(right)
                 return left * right
             case BinOp("/", left, right):
-                left = self.eval(left, environment)
-                right = self.eval(right, environment)
-                assert type(left) == type(right) == Fraction
+                left = self.eval(left)
+                right = self.eval(right)
                 return left / right
             case BinOp("==", left, right):
                 left = self.eval(left)
                 right = self.eval(right)
-                assert type(left) == type(right)
                 return left == right
             case BinOp("!=", left, right):
                 left = self.eval(left)
@@ -87,32 +89,26 @@ class RuntimeEnvironment():
             case BinOp("<", left, right):
                 left = self.eval(left)
                 right = self.eval(right)
-                assert type(left) == type(right)
                 return left < right
             case BinOp(">", left, right):
                 left = self.eval(left)
                 right = self.eval(right)
-                assert type(left) == type(right)
-                return self.eval(left) > self.eval(right)
+                return left > right
             case BinOp("<=", left, right):
                 left = self.eval(left)
                 right = self.eval(right)
-                assert type(left) == type(right)
-                return self.eval(left) <= self.eval(right)
+                return left <= right
             case BinOp(">=", left, right):
                 left = self.eval(left)
                 right = self.eval(right)
-                assert type(left) == type(right)
-                return self.eval(left) >= self.eval(right)
+                return left >= right
             case BinOp("&&", left, right):
                 left = self.eval(left)
                 right = self.eval(right)
-                assert type(left) == type(right) == bool
                 return left and right
             case BinOp("||", left, right):
                 left = self.eval(left)
                 right = self.eval(right)
-                assert type(left) == type(right) == bool
                 return left or right
             
             # Unary operation is the same, except for the operator.
@@ -121,17 +117,27 @@ class RuntimeEnvironment():
 
             # Again, If is different, so we define it separately.
             case If(cond, e1, e2):
-                assert type(self.eval(cond)) == bool
                 if self.eval(cond) == True:
-                    return self.eval(e1)
+                    self.scope = self.scope + 1
+                    val = self.eval(e1)
+                    self.scope = self.scope - 1
+                    return val
                 else:
-                    return self.eval(e2)
-            # for For_Loops
+                    self.scope = self.scope + 1
+                    val =  self.eval(e2)
+                    self.scope = self.scope - 1
+                    return val
+                        # for For_Loops
             case ForLoop(Variable(name), val_list, stat):
                 h = len(val_list)
-                for x in range(h):
+                for x in range(h): 
                     v1 = self.eval(val_list[x])
-                    m = self.eval(stat, self.environment | { name: v1 })
+                    self.scope = self.scope + 1
+                    dict = {name : v1}
+                    self.environment.append(dict)
+                    m = self.eval(stat)
+                    self.scope = self.scope - 1
+                    self.environment.pop()
                     if(x==(h-1)):
                         return(m)
-        raise InvalidProgram(f"Runtime environment does not support program: {program}.")
+        raise InvalidProgramError(program)
